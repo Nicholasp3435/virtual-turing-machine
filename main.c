833e3203f547fca2c610f696947146c3829baf5a
Nicholas <nap44794@uga.edu>
2025-05-02 21:18:32 -0400

    feature: add remove_white_spaces function

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 // main.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "input.h"
+
+char* remove_white_space(char* line) {
+
+    unsigned int line_len = strlen(line);
+
+    char* parsed_line = malloc(line_len * sizeof(char));
+    
+    unsigned int parsed_index = 0;
+    unsigned int line_index = 0;
+
+    while(parsed_index < line_len) {
+        if (line[line_index] != ' ') {
+            parsed_line[parsed_index] = line[line_index];
+            parsed_index++;
+        }
+        line_index++;
+    }
+
+    free(line);
+
+    return parsed_line;
+}
+
+
 
 int main(int argc, char** argv) {
 
@@ -10,7 +35,8 @@ int main(int argc, char** argv) {
     if (fp) {
         char* line;
         while ((line = read_line(fp)) != NULL) {
-            printf("%s", line);
+
+            char* parsed_line = remove_white_space(line);
             free(line); // Don't forget to free after use!
         }
         printf("\n");
@@ -20,3 +46,4 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
