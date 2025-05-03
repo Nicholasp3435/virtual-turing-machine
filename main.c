d691e97d3d1d531fc80dba4edfec2b477e4411b6
Nicholas <nap44794@uga.edu>
2025-05-02 21:49:03 -0400

    feature: add functions to parse input file

--- main.c
+++ main.c
@@ -2,22 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "input.h"
 
 char* remove_white_space(char* line) {
 
     unsigned int line_len = strlen(line);
 
-    char* parsed_line = malloc(line_len * sizeof(char));
+    char* parsed_line = calloc(line_len, sizeof(char));
     
     unsigned int parsed_index = 0;
     unsigned int line_index = 0;
+    bool is_in_quotes = false;
 
     while(parsed_index < line_len) {
-        if (line[line_index] != ' ') {
+        if (line[line_index] == '"') {
+            is_in_quotes = true;
+            line_index++;
+        } 
+
+        if (line[line_index] != ' ' || is_in_quotes) {
             parsed_line[parsed_index] = line[line_index];
             parsed_index++;
         }
+
+        if (line[line_index] == '"') {
+            is_in_quotes = false;
+            line_index++;
+        } 
+
         line_index++;
     }
 
@@ -25,8 +38,38 @@ char* remove_white_space(char* line) {
 
     return parsed_line;
 }
+char* remove_comments(char* line) {
 
+    unsigned int line_len = strlen(line);
 
+    char* parsed_line = calloc(line_len, sizeof(char));
+    
+    unsigned int parsed_index = 0;
+    unsigned int line_index = 0;
+    bool is_in_comment = false; 
+
+    while(parsed_index < line_len) {
+        if (line[line_index] == '/' && line[line_index + 1] == '*') {
+            is_in_comment = true;
+        } 
+
+        if (!is_in_comment) {
+            parsed_line[parsed_index] = line[line_index];
+            parsed_index++;
+        }
+
+        if (line[line_index + 1] == '/' && line[line_index] == '*') {
+            is_in_comment = false;
+            line_index++;
+        }
+
+        line_index++;
+    }
+
+    free(line);
+
+    return parsed_line;
+}
 
 int main(int argc, char** argv) {
 
@@ -36,8 +79,11 @@ int main(int argc, char** argv) {
         char* line;
         while ((line = read_line(fp)) != NULL) {
 
-            char* parsed_line = remove_white_space(line);
-            free(line); // Don't forget to free after use!
+            char* no_spaces = remove_white_space(line);
+            char* parsed_line = remove_comments(no_spaces);
+            printf("%s", parsed_line);
+
+            free(parsed_line);
         }
         printf("\n");
         fclose(fp);
--- sample.tm
+++ sample.tm
@@ -9,14 +9,12 @@ F  = {qa}                   /* Set of accepting states */
 B  = "_"                    /* The blank symbol */
 
 /* Below is the definition of the transition function */
-d(q0, 0) -> (q1, 0, R)
-d(q0, 1) -> (q1, 1, R)
-d(q0, _) -> (qr, _, R)
-d(q1, 0) -> (q2, 0, R)
-d(q1, 1) -> (q2, 1, R)
-d(q1, _) -> (qr, _, R)
-d(q2, 0) -> (qr, 0, R)
-d(q2, 1) -> (qr, 1, R)
-d(q2, _) -> (qa, _, R)
-
-/* Alternatively, because I get my left and right confused, {<, >} may be used in place of {L, R} */
+d(q0, 0) > (q1, 0, R)
+d(q0, 1) > (q1, 1, R)
+d(q0, _) > (qr, _, R)
+d(q1, 0) > (q2, 0, R)
+d(q1, 1) > (q2, 1, R)
+d(q1, _) > (qr, _, R)
+d(q2, 0) > (qr, 0, R)
+d(q2, 1) > (qr, 1, R)
+d(q2, _) > (qa, _, R)
