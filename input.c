eb3c774bd094ddcef81bd257bb0d93fdef29032a
Nicholas <nap44794@uga.edu>
2025-05-02 21:57:49 -0400

    refactor: move parsing functions to input.c

--- input.c
+++ input.c
@@ -1,27 +1,7 @@
 #include "input.h"
 #include <stdlib.h>
 #include <string.h>
-
-char* read_file_to_string(const char* filename) {
-    FILE* fp = fopen(filename, "r");
-    if (!fp) return NULL;
-
-    fseek(fp, 0, SEEK_END);
-    long size = ftell(fp);
-    rewind(fp);
-
-    char* buffer = malloc(size + 1);
-    if (!buffer) {
-        fclose(fp);
-        return NULL;
-    }
-
-    fread(buffer, 1, size, fp);
-    buffer[size] = '\0';
-
-    fclose(fp);
-    return buffer;
-}
+#include <stdbool.h>
 
 char* read_line(FILE* fp) {
     char* line = NULL;
@@ -33,5 +13,72 @@ char* read_line(FILE* fp) {
         return NULL;
     }
 
-    return line;  // caller must free this
+    return line;
+}
+
+char* remove_white_space(char* line) {
+
+    unsigned int line_len = strlen(line);
+
+    char* parsed_line = calloc(line_len, sizeof(char));
+    
+    unsigned int parsed_index = 0;
+    unsigned int line_index = 0;
+    bool is_in_quotes = false;
+
+    while(parsed_index < line_len) {
+        if (line[line_index] == '"') {
+            is_in_quotes = true;
+            line_index++;
+        } 
+
+        if (line[line_index] != ' ' || is_in_quotes) {
+            parsed_line[parsed_index] = line[line_index];
+            parsed_index++;
+        }
+
+        if (line[line_index] == '"') {
+            is_in_quotes = false;
+            line_index++;
+        } 
+
+        line_index++;
+    }
+
+    free(line);
+
+    return parsed_line;
+}
+
+char* remove_comments(char* line) {
+
+    unsigned int line_len = strlen(line);
+
+    char* parsed_line = calloc(line_len, sizeof(char));
+    
+    unsigned int parsed_index = 0;
+    unsigned int line_index = 0;
+    bool is_in_comment = false; 
+
+    while(parsed_index < line_len) {
+        if (line[line_index] == '/' && line[line_index + 1] == '*') {
+            is_in_comment = true;
+        } 
+
+        if (!is_in_comment) {
+            parsed_line[parsed_index] = line[line_index];
+            parsed_index++;
+        }
+
+        if (line[line_index + 1] == '/' && line[line_index] == '*') {
+            is_in_comment = false;
+            line_index++;
+        }
+
+        line_index++;
+    }
+
+    free(line);
+
+    return parsed_line;
 }
--- main.c
+++ main.c
@@ -2,74 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include "input.h"
-
-char* remove_white_space(char* line) {
-
-    unsigned int line_len = strlen(line);
-
-    char* parsed_line = calloc(line_len, sizeof(char));
-    
-    unsigned int parsed_index = 0;
-    unsigned int line_index = 0;
-    bool is_in_quotes = false;
-
-    while(parsed_index < line_len) {
-        if (line[line_index] == '"') {
-            is_in_quotes = true;
-            line_index++;
-        } 
-
-        if (line[line_index] != ' ' || is_in_quotes) {
-            parsed_line[parsed_index] = line[line_index];
-            parsed_index++;
-        }
-
-        if (line[line_index] == '"') {
-            is_in_quotes = false;
-            line_index++;
-        } 
-
-        line_index++;
-    }
-
-    free(line);
-
-    return parsed_line;
-}
-char* remove_comments(char* line) {
-
-    unsigned int line_len = strlen(line);
-
-    char* parsed_line = calloc(line_len, sizeof(char));
-    
-    unsigned int parsed_index = 0;
-    unsigned int line_index = 0;
-    bool is_in_comment = false; 
-
-    while(parsed_index < line_len) {
-        if (line[line_index] == '/' && line[line_index + 1] == '*') {
-            is_in_comment = true;
-        } 
-
-        if (!is_in_comment) {
-            parsed_line[parsed_index] = line[line_index];
-            parsed_index++;
-        }
-
-        if (line[line_index + 1] == '/' && line[line_index] == '*') {
-            is_in_comment = false;
-            line_index++;
-        }
-
-        line_index++;
-    }
-
-    free(line);
-
-    return parsed_line;
-}
 
 int main(int argc, char** argv) {
 
